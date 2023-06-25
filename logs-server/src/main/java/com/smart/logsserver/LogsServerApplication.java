8be7272e7bf5b6dbfcebaf7c97ec173656d00ba3
Viktor <lionvityalion@gmail.com>
2023-06-25 20:48:23 +0300

    Added SpringBoot Server

---
+++ logs-server/src/main/java/com/smart/logsserver/LogsServerApplication.java
@@ -0,0 +1,13 @@
+package com.smart.logsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogsServerApplication.class, args);
+	}
+
+}
---
+++ logs-server/src/test/java/com/smart/logsserver/LogsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.logsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
